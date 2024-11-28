075ce97fb416b602c0b32849a9d7837519c56044
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2024-11-28 01:05:08 +0100

    Initialize main to execute tasks

--- src/arduino/smart_waste_disposal/src/main.cpp
+++ src/arduino/smart_waste_disposal/src/main.cpp
@@ -1,18 +1,60 @@
 #include <Arduino.h>
+#include "config.h"
+#include "kernel/Scheduler.h"
+#include "kernel/Logger.h"
+#include "kernel/MsgService.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#include "model/UserConsole.h"
+#include "model/WasteDisposalSystem.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#include "tasks/WorkflowTask.h"
+#include "tasks/CheckTask.h"
+#include "tasks/FullnessTask.h"
+#include "tasks/TemperatureTask.h"
+#include "tasks/userPresenceTask.h"
+#include "tasks/TelemetryTask.h"
+
+Scheduler sched;
+
+WasteDisposalSystem *pSystem;
+UserConsole *pUserConsole;
+
+void setup()
+{
+    MsgService.init();
+    sched.init(100);
+
+    Logger.log(".:: Smart Waste Disposal System ::.");
+
+    pSystem = new WasteDisposalSystem();
+    pSystem->init();
+
+    pUserConsole = new UserConsole();
+    pUserConsole->init();
+
+    WorkflowTask *pWorkflowTask = new WorkflowTask(pSystem, pUserConsole);
+    // CheckTask *pCheckTask = new CheckTask(pSystem, pUserConsole);
+    FullnessTask *pFullnessTask = new FullnessTask(pSystem);
+    TemperatureTask *pTemperatureTask = new TemperatureTask(pSystem);
+    UserPresenceTask *pUserPresenceTask = new UserPresenceTask(pSystem);
+    // TelemetryTask *pTelemetryTask = new TelemetryTask(pSystem);
+
+    pWorkflowTask->init(100);
+    // pCheckTask->init(100);
+    pFullnessTask->init(100);
+    pTemperatureTask->init(100);
+    pUserPresenceTask->init(100);
+    // pTelemetryTask->init(100);
+
+    sched.addTask(pWorkflowTask);
+    // sched.addTask(pCheckTask);
+    sched.addTask(pFullnessTask);
+    sched.addTask(pTemperatureTask);
+    sched.addTask(pUserPresenceTask);
+    // sched.addTask(pTelemetryTask);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    sched.schedule();
 }
