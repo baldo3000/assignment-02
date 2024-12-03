12903d249461bd1006257c76561bfc621b11ab7b
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2024-12-03 22:45:11 +0100

    Improve log graphics

--- src/pc/SmartWasteDisposal/src/main/andreabaldazzi/smart_waste_disposal/LogView.java
+++ src/pc/SmartWasteDisposal/src/main/andreabaldazzi/smart_waste_disposal/LogView.java
@@ -1,8 +1,9 @@
 package andreabaldazzi.smart_waste_disposal;
 
 import java.util.Date;
-
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 class LogView extends JFrame {
 
@@ -14,11 +15,27 @@ class LogView extends JFrame {
 		setResizable(false);
 
 		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+
+		JLabel titleLabel = new JLabel("Log Messages", JLabel.CENTER);
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 16));
+		mainPanel.add(titleLabel, BorderLayout.NORTH);
+
 		log = new JTextArea(30, 40);
 		log.setEditable(false);
+		log.setFont(new Font("Monospaced", Font.PLAIN, 12));
+		log.setBorder(new EmptyBorder(10, 10, 10, 10));
 		JScrollPane scroll = new JScrollPane(log);
 		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		mainPanel.add(scroll);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroll.getVerticalScrollBar().setUI(new javax.swing.plaf.basic.BasicScrollBarUI() {
+			@Override
+			protected void configureScrollBarColors() {
+				this.thumbColor = Color.GRAY;
+			}
+		});
+		mainPanel.add(scroll, BorderLayout.CENTER);
+
 		this.getContentPane().add(mainPanel);
 	}
 
